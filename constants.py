dffd7713201482f13951151db04f96b0299facae
Keaton Drebes <keatondrebes@keatons-air.cate.org>
2018-04-15 16:14:55 -0700

    Keaton Koments konstants

--- constants.py
+++ constants.py
@@ -17,12 +17,25 @@ enemiesFPS = 60
 projectleFPS = 60
 
 def getImage(name):
+    """ Arguments: the name of the file
+    returns the absolute path to said file as a string
+    (Keaton's side note, doesn't this just do the exact same thing as os.path.abspath(filename)?
+    """
     current_path = os.path.dirname(__file__) 
     image_path = os.path.join(current_path, name) 
     return(image_path)
+
 def angleToVector(angle,size):
+    """arguments: the angle in radians, the magnitude of the vecto
+    returns a list with list[0] being the x component, and list[1] being the y component
+    """
     return [round(size*math.cos(angle),3),round(size*math.sin(angle),3)]
+
 def vectorToAngle(vector):
+    """Agruments: A list, list[0] represents the x component of the vector, list[1] represents the Y
+    if the x component of a the vector has a magnitude of less then 10, return pi/2.
+    else, return the angle in radians
+    """
     if abs(vector[0])-10<0:
         return math.pi/2
     if vector[0]>0:
